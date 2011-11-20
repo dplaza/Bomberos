60404c84f1a9daab759002da2a350cd06cf5f2bd
Rodrigo Pizarro <rodrigo1494@hotmail.com>
2011-11-20 12:14:12 -0300

    Hecho los windows forms de Compañia.cs Cursos.cs Premios.cs y Cargos.cs

---
+++ Bomberos.Presentacion/Compa�ia.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Bomberos.Presentacion
+{
+    partial class Compañia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.txt_nombres = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridResult = new System.Windows.Forms.DataGridView();
+            this.idCompañia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreCompañia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_borrar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            this.btn_editar = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_agregar);
+            this.groupBox2.Controls.Add(this.txt_nombres);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(675, 81);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Agregar Compañia";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(486, 36);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(75, 23);
+            this.btn_agregar.TabIndex = 14;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            // 
+            // txt_nombres
+            // 
+            this.txt_nombres.Location = new System.Drawing.Point(123, 38);
+            this.txt_nombres.Name = "txt_nombres";
+            this.txt_nombres.Size = new System.Drawing.Size(272, 20);
+            this.txt_nombres.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre Compañia";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridResult);
+            this.groupBox1.Location = new System.Drawing.Point(11, 133);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(678, 287);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Compañias";
+            // 
+            // dataGridResult
+            // 
+            this.dataGridResult.AllowUserToAddRows = false;
+            this.dataGridResult.AllowUserToDeleteRows = false;
+            this.dataGridResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idCompañia,
+            this.NombreCompañia});
+            this.dataGridResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridResult.Location = new System.Drawing.Point(3, 16);
+            this.dataGridResult.Name = "dataGridResult";
+            this.dataGridResult.ReadOnly = true;
+            this.dataGridResult.Size = new System.Drawing.Size(672, 268);
+            this.dataGridResult.TabIndex = 0;
+            this.dataGridResult.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridResult_CellContentClick);
+            // 
+            // idCompañia
+            // 
+            this.idCompañia.DataPropertyName = "idCompañia";
+            this.idCompañia.DividerWidth = 1;
+            this.idCompañia.HeaderText = "idCompañia";
+            this.idCompañia.Name = "idCompañia";
+            this.idCompañia.ReadOnly = true;
+            this.idCompañia.Width = 70;
+            // 
+            // NombreCompañia
+            // 
+            this.NombreCompañia.DataPropertyName = "NombreCompañia";
+            this.NombreCompañia.DividerWidth = 1;
+            this.NombreCompañia.HeaderText = "                                                             Nombre Compañia";
+            this.NombreCompañia.Name = "NombreCompañia";
+            this.NombreCompañia.ReadOnly = true;
+            this.NombreCompañia.Width = 560;
+            // 
+            // btn_borrar
+            // 
+            this.btn_borrar.Location = new System.Drawing.Point(527, 426);
+            this.btn_borrar.Name = "btn_borrar";
+            this.btn_borrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_borrar.TabIndex = 22;
+            this.btn_borrar.Text = "Editar";
+            this.btn_borrar.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_cancelar.Location = new System.Drawing.Point(608, 426);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancelar.TabIndex = 23;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // btn_editar
+            // 
+            this.btn_editar.Location = new System.Drawing.Point(446, 426);
+            this.btn_editar.Name = "btn_editar";
+            this.btn_editar.Size = new System.Drawing.Size(75, 23);
+            this.btn_editar.TabIndex = 24;
+            this.btn_editar.Text = "button1";
+            this.btn_editar.UseVisualStyleBackColor = true;
+            // 
+            // Compañia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 448);
+            this.Controls.Add(this.btn_editar);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_borrar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Compañia";
+            this.Text = "Compañia";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.TextBox txt_nombres;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idCompañia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreCompañia;
+        private System.Windows.Forms.Button btn_borrar;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.Button btn_editar;
+    }
+}
---
+++ Bomberos.Presentacion/Cursos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bomberos.Presentacion
+{
+    public partial class Cursos : Form
+    {
+        public Cursos()
+        {
+            InitializeComponent();
+        }
+
+        private void Cursos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
