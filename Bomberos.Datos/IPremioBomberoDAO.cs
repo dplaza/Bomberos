61df3df1cc4395c8f23a49f0e34db56c6545a9be
Rodrigo <rodrigo1494@hotmail.com>
2011-11-24 15:25:43 -0300

    Listo IPremioBomberoDAO.cs

---
+++ Bomberos.Datos/IPremioBomberoDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bomberos.Comun;
+
+namespace Bomberos.Datos
+{
+    public class IPremioBomberoDAO
+    {
+        bool Insert(PremioBomberoDTO p_PremioBombero);
+        PremioBomberoDTO Load(PremioBomberoDTO p_PremioBombero);
+        bool Delete(PremioBomberoDTO p_PremioBombero);
+        bool Update(PremioBomberoDTO p_PremioBombero);
+        List<PremioBomberoDTO> LoadAll();
+    }
+}
